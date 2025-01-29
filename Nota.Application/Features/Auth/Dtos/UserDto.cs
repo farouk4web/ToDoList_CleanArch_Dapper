492c23747249dbd644d7da4295be7f022d47ff70
Farouk Abdelhamid <faroukAbdelhamid.eg@gmail.com>
2025-01-30 01:34:14 +0200

    feat: create Custom Mapping Class And User Dto

---
+++ Nota.Application/Features/Auth/Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Nota.Application.Features.Auth.Dtos
+{
+    public class UserDto
+    {
+        public string? FullName { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ Nota.Application/Mapping/UserMapping.cs
@@ -0,0 +1,27 @@
+﻿using Nota.Application.Features.Auth.Dtos;
+using Nota.Domain.Entities;
+
+namespace Nota.Application.Mapping
+{
+    public static class UserMapping
+    {
+        public static User ToDomain(this UserDto dto)
+        {
+            return new User
+            {
+                FullName = dto.FullName,
+                UserName = dto.UserName,
+                HashPassword = dto.Password
+            };
+        }
+
+        public static UserDto ToDto(this User dto)
+        {
+            return new UserDto
+            {
+                FullName = dto.FullName,
+                UserName = dto.UserName,
+            };
+        }
+    }
+}
