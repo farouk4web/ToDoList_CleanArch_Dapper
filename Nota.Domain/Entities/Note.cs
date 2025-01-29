25960a3a9a010bfe6e0fae103357ada065b0725d
Farouk Abdelhamid <faroukAbdelhamid.eg@gmail.com>
2025-01-30 01:05:08 +0200

    feat: Add priority Field Into Notes

--- Nota.Domain/Entities/Note.cs
+++ Nota.Domain/Entities/Note.cs
@@ -1,9 +1,12 @@
-﻿namespace Nota.Domain.Entities
+﻿using Nota.Domain.Enums;
+
+namespace Nota.Domain.Entities
 {
     public class Note : BaseEntity
     {
         public string? Title { get; set; }
         public string? Content { get; set; }
         public string? Color { get; set; }
+        public Priority Priority { get; set; }
     }
 }
---
+++ Nota.Domain/Enums/Priority.cs
@@ -0,0 +1,10 @@
+﻿namespace Nota.Domain.Enums
+{
+    public enum Priority
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3,
+        Critical = 4
+    }
+}
